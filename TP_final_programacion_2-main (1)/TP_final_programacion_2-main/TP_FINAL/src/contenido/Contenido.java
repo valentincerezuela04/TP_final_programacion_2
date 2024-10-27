189b37dee2befad7b5a08b281a606bc413f9a78d
Valentin Cerezuela <123900495+valentincerezuela04@users.noreply.github.com>
2024-10-27 15:29:05 -0300

    commit 1 de esta branch
    restructurando contenido de la clase contenido y sus hijas anime y manga 
    -Json utils clase abst con sus hijas JsonUtilAnime y JsonUtilManga (mas tarde abria que hacer un JsonUtilUser)
    -Consumiendo la api y guardando los datos  de Anime en un archivo(hay que hacer lo mismo con Manga) 
    -hay que terminar de acomodar el tema del archivo de la api por que se guarda bien pero nose muestra bien

---
+++ TP_final_programacion_2-main (1)/TP_final_programacion_2-main/TP_FINAL/src/contenido/Contenido.java
@@ -0,0 +1,121 @@
+package contenido;
+
+import java.util.Objects;
+
+public abstract class Contenido {
+    private int id;
+    private String name;
+    private String urL_image;
+    private String title;
+    private String status;
+    private double score;
+    private int rank;
+    private int popularity;
+    private int members;
+    private String synopsis;
+    private Estado_vistoONo_enum vistoONo;
+
+    public Contenido(int id, int members, String name, int popularity, int rank, double score, String status, String synopsis, String title, String urL_image, Estado_vistoONo_enum vistoONo) {
+        this.id = id;
+        this.members = members;
+        this.name = name;
+        this.popularity = popularity;
+        this.rank = rank;
+        this.score = score;
+        this.status = status;
+        this.synopsis = synopsis;
+        this.title = title;
+        this.urL_image = urL_image;
+        this.vistoONo = Estado_vistoONo_enum.NO_VISTO;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMembers() {
+        return members;
+    }
+
+    public void setMembers(int members) {
+        this.members = members;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(int popularity) {
+        this.popularity = popularity;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrL_image() {
+        return urL_image;
+    }
+
+    public void setUrL_image(String urL_image) {
+        this.urL_image = urL_image;
+    }
+
+    public Estado_vistoONo_enum getVistoONo() {
+        return vistoONo;
+    }
+
+    public void setVistoONo(Estado_vistoONo_enum vistoONo) {
+        this.vistoONo = vistoONo;
+    }
+
+    public abstract void visto_o_no();
+}
